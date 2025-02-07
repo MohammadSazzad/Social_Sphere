fc5fc673ea669fcaccdde9587330726cd166c92a
MohammadSazzad <sazzad19@student.sust.edu>
2025-02-07 21:48:55 +0600

    Initialization

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
