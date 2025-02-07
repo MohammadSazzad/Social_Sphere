00ede7ee2b305439281459d1489ee42f5e7187f5
MohammadSazzad <sazzad19@student.sust.edu>
2025-02-07 22:32:33 +0600

    Connecting the database

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import usersRouter from './route/users.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.SERVER_PORT;
 
 const app = express();
+
+app.use(express.json());
+app.use('/api/users', usersRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
     }
 );
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     }
 );
