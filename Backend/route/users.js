a33c956f52fc2e9711a9518bdc8c3b3c5b206f52
MohammadSazzad <sazzad19@student.sust.edu>
2025-02-09 21:09:31 +0600

    VerifyUser and createing JWT Token

--- Backend/route/users.js
+++ Backend/route/users.js
@@ -1,10 +1,11 @@
-import { getUsersController, signUpController } from "../controller/users.js";
+import { getUsersController, signUpController, verifyUserController } from "../controller/users.js";
 import express from "express";
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", getUsersController);
 usersRouter.post("/signup", signUpController);
+usersRouter.post("/verify", verifyUserController);
 
 
 export default usersRouter;
