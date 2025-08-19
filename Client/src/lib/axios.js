bc0782f0c5d00defd3aae7550d72e38fb7964b05
MohammadSazzad <sazzad19@student.sust.edu>
2025-08-19 19:25:56 +0600

    fix bug

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://socialsphere.eastasia.cloudapp.azure.com:3000/api';
+const API_BASE_URL = 'http://socialsphere.eastasia.cloudapp.azure.com/api';
 
 export const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
